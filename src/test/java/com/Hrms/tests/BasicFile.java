25a776c66034c5b5599516f8afe0c89c9873493d
Jagadish <Jagadish@192.168.1.46>
2025-01-08 22:51:58 +0530

    Commit the local code to repository

---
+++ src/test/java/com/Hrms/tests/BasicFile.java
@@ -0,0 +1,11 @@
+package com.Hrms.tests;
+
+public class BasicFile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String G ="Welcome to GitHub";
+		System.out.println(G);
+	}
+
+}
